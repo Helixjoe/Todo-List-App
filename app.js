4ffbc87ca79b727fe719e14b22e6b59750025238
Joel John <joeljohn5112@gmail.com>
2023-03-07 12:45:51 +0530

    day tracker updated

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const app = express();
 //body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const app = express();
 
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  req.send("hello");
+  let today = new Date();
+  let currentDay = today.getDay();
+  let day = "";
+  if (currentDay === 1) {
+    day = "Monday";
+  } else if (currentDay === 2) {
+    day = "Tuesday";
+  } else if (currentDay === 3) {
+    day = "Wednesday";
+  } else if (currentDay === 4) {
+    day = "Thursday";
+  } else if (currentDay === 5) {
+    day = "Friday";
+  } else if (currentDay === 6) {
+    day = "Saturday";
+  } else if (currentDay === 0) {
+    day = "Sunday";
+  }
+  res.render("list", { kindOfDay: day });
 });
 app.listen(3000, function () {
   console.log("Server started at port 3000");
